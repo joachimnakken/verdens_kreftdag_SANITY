0904d6cad706891d3b888abd2074f9dacebe0b99
joachimnakken <joachim@godtlevert.no>
2021-01-24 10:53:51 +0100

    ...

--- schemas/agenda/agenda.js
+++ schemas/agenda/agenda.js
@@ -13,25 +13,7 @@ export default {
       title: "Main title",
       type: "string",
     },
-    // {
-    //   name: "timeSchedule",
-    //   title: "Time schedule",
-    //   type: "array",
-    //   of: [
-    //     {
-    //       name: "time",
-    //       title: "Time",
-    //       description: "12:00 - 12:15 ",
-    //       type: "string",
-    //     },
-    //     {
-    //       name: "event",
-    //       title: "Event",
-    //       description: "What is happening?",
-    //       type: "string",
-    //     },
-    //   ],
-    // },
+
     {
       title: "Schedule",
       name: "schedule",
@@ -51,16 +33,15 @@ export default {
 
             {
               title: "Tidspunkt",
-              description: "format 09:45-10:00",
+              description: "format 09:45 - 10:00",
               name: "time",
               type: "string",
             },
           ],
           preview: {
             select: {
-              day: "day",
-              open: "open",
-              hours: "hours",
+              title: "title",
+              subtitle: "time",
             },
           },
         },
